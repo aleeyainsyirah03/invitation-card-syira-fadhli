00a81d416e112bb506bf0f252e7f35917161a0ad
Aleeya Insyirah <aleeyainsyirah03@yahoo.com>
2026-08-19 00:14:52 +0800

    Initial wedding invitation

---
+++ script.js
@@ -0,0 +1,12 @@
+const openButton =
+  document.getElementById("openInvitation");
+
+const envelope =
+  document.querySelector(".envelope");
+
+
+openButton.addEventListener("click", function () {
+
+  envelope.classList.add("open");
+
+});
